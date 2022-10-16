391268a34b426a8423393454b12e9fdfced595a2
Aji Pujo Hardiyanto <ajipujohardiyanto@gmail.com>
2022-10-16 14:34:49 +0700

    api

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,12 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+  let data = {
+    npm: 2022320048,
+    nama: "Aji Pujo Hardiyanto",
+    kelas: "SI20B",
+    Angkatan: 2020,
+  };
+
+  res.status(200).json(data);
 }
